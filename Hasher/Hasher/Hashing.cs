d709c42bffccb455f402d6adaa5453373ef212c0
Nephithys <Nephithys@gmail.com>
2016-01-06 18:11:48 -0600

    created hasing class and list class, aswell as a working pasword hasher

---
+++ Hasher/Hasher/Hashing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hasher
+{
+    class Hashing
+    {
+        public string getUseInput()
+        {
+            Console.WriteLine("Enter a password");
+            string useString = Console.ReadLine();
+            return useString;
+        }
+
+        public int hash(string useString)
+            {
+                int res = 0;
+                foreach (char c in useString)
+                {
+                    int d = c - 'A' + 1;
+                    res = 26 * res + d;
+                }
+                return res;
+            }
+
+        public void resultingHash(string useString)
+        {
+            
+
+            var ss = new[] { useString };
+            foreach (var s in ss)
+            {
+                Console.WriteLine("{0} - {1}", s, hash(s));
+            }
+        }
+
+        
+    }
+}
---
+++ Hasher/Hasher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Hasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashing newHashing = new Hashing();
+            string getUI = newHashing.getUseInput();
+            int printInt = newHashing.hash(getUI);
+            newHashing.resultingHash(getUI);
+            
+
+            //var awesomeList = new CustomDictionary<string>();
+
+            //{ awesomeList = getUI[printInt]; }
+
+        }
+    }
+}
